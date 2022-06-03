2488bf159e50e84a81fe9e1f218acd725fbe6084
HaloSense <liuzj990412@live.com>
2022-06-02 20:51:52 -0400

    1

--- Start/day01/HelloWorld.java
+++ Start/day01/HelloWorld.java
@@ -1,3 +1,19 @@
-class HelloWorld{
+/**
+Document comment
+@author Zijie Liu
+@version v1.0
+*/
 
+public class HelloChina{
+    public static void main(String[] args){
+        // Output to console
+        System.out.println("Hello, World!");
+    }
+    // Single line comment
+
+    /*
+    Multiple line comment
+    Multiple line comment
+    Multiple line comment
+    */
 }
